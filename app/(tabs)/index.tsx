5d5c2f8375eee5516fac2d0f868cdb90b3a09f03
Renz Cole <renz2arjhay@gmail.com>
2025-05-01 15:32:44 +0800

    feat: add Image component with responsive sizing and platform-specific styles

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,15 +1,35 @@
+import Header from "@/components/header";
 import { Text, View } from "react-native";
+import * as ImagePicker from "expo-image-picker";
+import { useState } from "react";
+import { Image } from "@/components/ui/image";
+import { Button, ButtonText } from "@/components/ui/button";
 
 export default function Index() {
+  const [image, setImage] = useState<string | null>(null);
+
+  const pickImage = async () => {
+    const result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ["images"],
+      quality: 1,
+    });
+
+    console.log(result);
+
+    if (!result.canceled) {
+      setImage(result.assets[0].uri);
+    }
+  };
+
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text className="font-nunito-regular">Tools</Text>
+    <View className="flex-1">
+      <Header />
+      <View className="flex-12">
+        <Button size="md" onPress={pickImage}>
+          <ButtonText>Pick an image.</ButtonText>
+        </Button>
+        {image && <Image source={{ uri: image }} />}
+      </View>
     </View>
   );
 }
--- components/header.tsx
+++ components/header.tsx
@@ -1,10 +1,19 @@
 import { View, Text } from "react-native";
 import React from "react";
+import { Image } from "./ui/image";
 
 const Header = () => {
   return (
-    <View>
-      <Text>Header</Text>
+    <View className="w-full p-3 bg-white elevation-md">
+      <View className="w-[70px]">
+        <Image
+          size="none"
+          className="h-[30px]"
+          resizeMode="contain"
+          source={require("@/assets/images/utilix-trans.png")}
+          alt="logo"
+        />
+      </View>
     </View>
   );
 };
