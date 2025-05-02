37edfd29b11b1816c9923e2b82b404e14173b10c
Renz Cole <renz2arjhay@gmail.com>
2025-05-02 11:07:48 +0800

    feat: enhance Img2Link screen with image selection and link generation functionality

--- app/(tabs)/(screens)/img2link.tsx
+++ app/(tabs)/(screens)/img2link.tsx
@@ -1,10 +1,12 @@
 import Header from "@/components/header";
 import { Button, ButtonText } from "@/components/ui/button";
 import { Card } from "@/components/ui/card";
-import { ScrollView, Text, View } from "react-native";
+import { Pressable, ScrollView, Text, View } from "react-native";
 import * as ImagePicker from "expo-image-picker";
 import { useState } from "react";
 import { Image } from "@/components/ui/image";
+import { Icon } from "@/components/ui/icon";
+import { FontAwesome5 } from "@expo/vector-icons";
 
 export default function Img2Link() {
   const [image, setImage] = useState<string | null>(null);
@@ -25,12 +27,13 @@ export default function Img2Link() {
     <View style={{ flex: 1 }}>
       <Header />
       <ScrollView className="px-4">
-        <Text className="mt-4 text-xl font-nunito-bold">Img2Link</Text>
+        {/* <Text className="mt-4 text-xl font-nunito-bold">Img2Link</Text> */}
 
         <Card className="mt-3 elevation">
           <Text className="text-base font-nunito-regular">
-            Img2Link is a powerful tool that allows you to convert images into
-            shareable links.
+            <Text className="font-nunito-bold text-lg">Img2Link</Text> is a
+            powerful tool that allows you to convert images into shareable links
+            using imgur.
           </Text>
           <Text className="text-base font-nunito-regular mt-2">
             Simply upload your image, and we'll generate a link that you can
@@ -38,37 +41,57 @@ export default function Img2Link() {
             attachments!
           </Text>
 
-          <Button className="mt-4" onPress={pickImage}>
+          {/* <Button className="mt-4" onPress={pickImage}>
             <ButtonText className="font-nunito-bold">Upload Image</ButtonText>
-          </Button>
-        </Card>
-
-        <Card className="mt-3 elevation">
-          <Text className="text-base font-nunito-regular">
-            Img2Link is a powerful tool that allows you to convert images into
-            shareable links.
-          </Text>
-          <Text className="text-base font-nunito-regular mt-2">
-            Simply upload your image, and we'll generate a link that you can
-            share with anyone. No more worrying about file sizes or email
-            attachments!
-          </Text>
-
-          <Button className="mt-4" onPress={pickImage}>
-            <ButtonText className="font-nunito-bold">Upload Image</ButtonText>
-          </Button>
+          </Button> */}
         </Card>
 
         <Card className="my-4 elevation">
-          {image && (
-            <Image
-              size="full"
-              className="h-60"
-              resizeMode="contain"
-              source={{ uri: image }}
-              alt="img"
-            />
+          {image ? (
+            <View className="rounded bg-gray-200">
+              <Image
+                size="full"
+                className="h-60"
+                resizeMode="contain"
+                source={{ uri: image }}
+                alt="img"
+              />
+            </View>
+          ) : (
+            <Pressable
+              onPress={pickImage}
+              className="h-60 justify-center items-center bg-gray-200 rounded"
+            >
+              <Text className="text-lg font-nunito-semibold">
+                Choose image from camera roll.
+              </Text>
+            </Pressable>
           )}
+          <View className="mt-4">
+            <Button disabled={!image} className={`${!image && "bg-gray-100"}`}>
+              <ButtonText
+                className={`font-nunito-regular ${!image && "text-gray-400"}`}
+              >
+                Generate Link
+              </ButtonText>
+            </Button>
+          </View>
+        </Card>
+        <Card className="mb-4 elevation">
+          <View className="w-full flex-row justify-between items-center rounded bg-gray-100">
+            <View className="w-9 p-2 items-center justify-center bg-gray-200 rounded-l">
+              <FontAwesome5 name="link" size={18} color="black" />
+            </View>
+            <Text
+              className="font-nunito-regular flex-1 px-1.5"
+              numberOfLines={1}
+            >
+              http://yourlink.comhttp://yourlink.comhttp://yourlink.comhttp://yourlink.com
+            </Text>
+            <View className="w-9 p-2 items-center justify-center bg-gray-200 rounded-r">
+              <FontAwesome5 name="copy" size={18} />
+            </View>
+          </View>
         </Card>
       </ScrollView>
     </View>
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -3,15 +3,20 @@ import "@/global.css";
 import { GluestackUIProvider } from "@/components/ui/gluestack-ui-provider";
 import "../global.css";
 import { SafeAreaView } from "react-native-safe-area-context";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
 
 export default function RootLayout() {
   return (
     <GluestackUIProvider mode="light">
-      <SafeAreaView className="flex-1">
-        <Stack>
-          <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        </Stack>
-      </SafeAreaView>
+      <QueryClientProvider client={queryClient}>
+        <SafeAreaView className="flex-1">
+          <Stack>
+            <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+          </Stack>
+        </SafeAreaView>
+      </QueryClientProvider>
     </GluestackUIProvider>
   );
 }
