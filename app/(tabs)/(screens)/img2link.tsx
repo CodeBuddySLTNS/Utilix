e98e50aca9a8ca5ee0b7387b7876f4443a6fd6cc
Renz Cole <renz2arjhay@gmail.com>
2025-05-02 16:40:56 +0800

    feat: implement image upload and link generation in Img2Link screen

--- app/(tabs)/(screens)/img2link.tsx
+++ app/(tabs)/(screens)/img2link.tsx
@@ -1,26 +1,62 @@
 import Header from "@/components/header";
 import { Button, ButtonText } from "@/components/ui/button";
 import { Card } from "@/components/ui/card";
-import { Pressable, ScrollView, Text, View } from "react-native";
+import { Alert, Pressable, ScrollView, Text, View } from "react-native";
 import * as ImagePicker from "expo-image-picker";
+import * as FileSystem from "expo-file-system";
+import * as Clipboard from "expo-clipboard";
 import { useState } from "react";
 import { Image } from "@/components/ui/image";
-import { Icon } from "@/components/ui/icon";
 import { FontAwesome5 } from "@expo/vector-icons";
+import axios from "axios";
+
+// 01c66d90e75c45c
 
 export default function Img2Link() {
-  const [image, setImage] = useState<string | null>(null);
+  const [image, setImage] = useState<ImagePicker.ImagePickerAsset | null>(null);
+  const [link, setLink] = useState<string | null>(null);
 
   const pickImage = async () => {
     const result = await ImagePicker.launchImageLibraryAsync({
       mediaTypes: ["images"],
       selectionLimit: 1,
-      quality: 1,
+      base64: true,
+      quality: 0.1,
     });
 
     if (!result.canceled) {
-      setImage(result.assets[0].uri);
+      setImage(result.assets[0]);
     }
+  };
+
+  const generateLink = async (uri: string | null | undefined) => {
+    if (!uri) return;
+
+    const base64 = await FileSystem.readAsStringAsync(uri, {
+      encoding: FileSystem.EncodingType.Base64,
+    });
+    const response = await axios.post(
+      "https://api.imgur.com/3/image",
+      { image: base64, type: "base64" },
+      {
+        headers: {
+          Authorization: "Client-ID 01c66d90e75c45c",
+          "Content-Type": "application/json",
+        },
+      }
+    );
+    if (response.status === 200) {
+      setLink(response.data.data.link);
+    }
+    if (response.status !== 200) {
+      setLink(null);
+    }
+    setImage(null);
+  };
+
+  const copyToClipboard = async (text: string) => {
+    await Clipboard.setStringAsync(text);
+    Alert.alert("Copied to clipboard", text);
   };
 
   return (
@@ -40,23 +76,18 @@ export default function Img2Link() {
             share with anyone. No more worrying about file sizes or email
             attachments!
           </Text>
-
-          {/* <Button className="mt-4" onPress={pickImage}>
-            <ButtonText className="font-nunito-bold">Upload Image</ButtonText>
-          </Button> */}
         </Card>
 
         <Card className="my-4 elevation">
           {image ? (
-            <View className="rounded bg-gray-200">
+            <Pressable onPress={pickImage} className="h-60 rounded bg-gray-200">
               <Image
                 size="full"
-                className="h-60"
                 resizeMode="contain"
-                source={{ uri: image }}
+                source={{ uri: image.uri }}
                 alt="img"
               />
-            </View>
+            </Pressable>
           ) : (
             <Pressable
               onPress={pickImage}
@@ -68,7 +99,11 @@ export default function Img2Link() {
             </Pressable>
           )}
           <View className="mt-4">
-            <Button disabled={!image} className={`${!image && "bg-gray-100"}`}>
+            <Button
+              disabled={!image}
+              onPress={() => generateLink(image?.uri)}
+              className={`${!image && "bg-gray-100"}`}
+            >
               <ButtonText
                 className={`font-nunito-regular ${!image && "text-gray-400"}`}
               >
@@ -78,20 +113,29 @@ export default function Img2Link() {
           </View>
         </Card>
         <Card className="mb-4 elevation">
-          <View className="w-full flex-row justify-between items-center rounded bg-gray-100">
-            <View className="w-9 p-2 items-center justify-center bg-gray-200 rounded-l">
-              <FontAwesome5 name="link" size={18} color="black" />
+          {link ? (
+            <View className="w-full flex-row justify-between items-center rounded bg-gray-100">
+              <View className="w-9 p-2 items-center justify-center bg-gray-200 rounded-l">
+                <FontAwesome5 name="link" size={18} color="black" />
+              </View>
+              <Text
+                className="font-nunito-regular flex-1 px-1.5"
+                numberOfLines={1}
+              >
+                {link}
+              </Text>
+              <Pressable
+                onPress={() => copyToClipboard(link)}
+                className="w-9 p-2 items-center justify-center bg-gray-200 rounded-r"
+              >
+                <FontAwesome5 name="copy" size={18} />
+              </Pressable>
             </View>
-            <Text
-              className="font-nunito-regular flex-1 px-1.5"
-              numberOfLines={1}
-            >
-              http://yourlink.comhttp://yourlink.comhttp://yourlink.comhttp://yourlink.com
+          ) : (
+            <Text className="font-nunito-regular text-gray-500 text-center">
+              Your link will be generated here.
             </Text>
-            <View className="w-9 p-2 items-center justify-center bg-gray-200 rounded-r">
-              <FontAwesome5 name="copy" size={18} />
-            </View>
-          </View>
+          )}
         </Card>
       </ScrollView>
     </View>
